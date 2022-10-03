6147e96868c31fba45aaebda1aa1e54aab98c879
Ammalu999 <114848889+Ammalu999@users.noreply.github.com>
2022-10-03 10:44:07 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,10 +33,10 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['916282358482',''] //owner number +27634090203
+global.wame = ['916282358482'] 
+global.pemilik = ['916282358482'] //owner number +27634090203
+global.premium = ['916282358482'] //premium number +27634090203
 global.pengguna = 'Drips' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
